936af0f6bb354a2a0133a8d65556e3202c12a763
MYwizza <35356142+MYwizza@users.noreply.github.com>
2018-05-11 16:16:36 +0800

    update
    just for update

--- titanic_datadeal.py
+++ titanic_datadeal.py
@@ -416,5 +416,54 @@ stackingsubmission=pd.DataFrame({'PassengersId':PassengerId,'Survived':predictio
 stackingsubmission.to_csv('stacksubmission.csv',index=False,sep=',')
 
 
+#构建学习曲线
+from sklearn.learning_curve import learning_curve
+
+def plot_learning_curve(estimator,title,x,y,ylim=None,cv=None,n_jobs=1,train_sizes=np.linspace(0.1,1,5),verbose=0):
+    plt.figure()
+    plt.title('title')
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel('training examples')
+    plt.ylabel('score')
+    train_sizes,train_scores,test_scores=learning_curve(estimator,x,y,cv=cv,n_jobs=n_jobs,train_sizes=train_sizes)
+    train_scores_mean=np.mean(train_scores,axis=1)
+    train_scores_std=np.std(train_scores,axis=1)
+    test_scores_mean=np.mean(test_scores,axis=1)
+    test_scores_std=np.std(test_scores,axis=1)
+    plt.grid()
+    
+    plt.fill_between(train_sizes,train_scores_mean-train_scores_std,
+                     train_scores_mean+train_scores_std,alpha=0.1,color='r')
+    plt.fill_between(train_sizes,test_scores_mean-test_scores_std,
+                     test_scores_mean+test_scores_std,alpha=0.1,color='r')
+    plt.plot(train_sizes,train_scores_mean,'o-',color='r',label='training score')
+    plt.plot(train_sizes,test_scores_mean,'o-',color='g',label='cross-validation score')
+    plt.legend(loc='best')
+    return plt
+
+#逐一观察不同模型的学习曲线
+rf_parameters = {'n_jobs': -1, 'n_estimators': 500, 'warm_start': True, 'max_depth': 6, 'min_samples_leaf': 2, 
+                 'max_features' : 'sqrt','verbose': 0}
+# AdaBoost
+ada_parameters = {'n_estimators':500, 'learning_rate':0.1}
+# ExtraTrees
+et_parameters = {'n_jobs': -1, 'n_estimators':500, 'max_depth': 8, 'min_samples_leaf': 2, 'verbose': 0}
+# GradientBoosting
+gb_parameters = {'n_estimators': 500, 'max_depth': 5, 'min_samples_leaf': 2, 'verbose': 0}
+# DecisionTree
+dt_parameters = {'max_depth':8}
+# KNeighbors
+knn_parameters = {'n_neighbors':2}
+# SVM
+svm_parameters = {'kernel':'linear', 'C':0.025}
+# XGB
+gbm_parameters = {'n_estimators': 2000, 'max_depth': 4, 'min_child_weight': 2, 'gamma':0.9, 'subsample':0.8, 
+                  'colsample_bytree':0.8, 'objective': 'binary:logistic', 'nthread':-1, 'scale_pos_weight':1}
+
+title='learning curve'
+plot_learning_curve(RandomForestClassifier(**rf_parameters),title,x_train,y_train,cv=None,n_jobs=4,
+                    train_sizes=np.linspace(50,500,10).astype(int))
+plt.show()
 
 
