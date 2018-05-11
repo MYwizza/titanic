bbaf299cd0c1589d5cac7efc1416c690cbdf727e
MYwizza <35356142+MYwizza@users.noreply.github.com>
2018-05-11 16:00:38 +0800

    update
    just for update

--- titanic_datadeal.py
+++ titanic_datadeal.py
@@ -339,3 +339,82 @@ plt.yticks(pos,ada_feature_imp['feature'][::-1])
 plt.xlabel('Relative importance')
 plt.title('AdaBoost feature importance')
 plt.show()
+
+#采用stacking方法，第一层模型使用KFold方法
+from sklearn.model_selection import KFold
+
+ntrain=titanic_train_data_x.shape[0]
+ntest=titanic_test_data_x.shape[0]
+seed=0
+nfolds=7
+kf=KFold(n_splits=nfolds,random_state=seed,shuffle=False)
+
+def get_out_fold(clf,x_train,y_train,x_test):
+    oof_train=np.zeros(ntrain)
+    oof_test=np.zeros(ntest)
+    oof_test_skf=np.empty((nfolds,ntest))
+    
+    for i, (train_index,test_index) in enumerate(kf.split(x_train)):
+        x_tr=x_train[train_index]
+        y_tr=y_train[train_index]
+        x_te=x_train[test_index]
+        clf.fit(x_tr,y_tr)
+        oof_train[test_index]=clf.predict(x_te)
+        oof_test_skf[i,:]=clf.predict(x_test)
+    
+    oof_test[:]=oof_test_skf.mean(axis=0)
+    return oof_train.reshape(-1,1),oof_test.reshape(-1,1)
+
+#构建不同的基学习器，使用RandomForest、AdaBoost、ExtraTrees、GBDT、DecisionTree、KNN、SVM七种基学习器
+    
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
+rf=RandomForestClassifier(n_estimators=500,warm_start=True,max_features='sqrt',max_depth=6,min_samples_split=3,
+                          min_samples_leaf=2,n_jobs=1,verbose=0)
+ada=AdaBoostClassifier(n_estimators=500,learning_rate=0.1)
+
+et=ExtraTreesClassifier(n_estimators=500,n_jobs=-1,max_depth=8,min_samples_leaf=2,verbose=0)
+
+gb=GradientBoostingClassifier(n_estimators=500,learning_rate=0.008,min_samples_split=3,min_samples_leaf=2,
+                              max_depth=5,verbose=0)
+
+dt=DecisionTreeClassifier(max_depth=8)
+
+knn=KNeighborsClassifier(n_neighbors=2)
+
+svm=SVC(kernel='linear',C=0.025)
+
+#将训练数据从Pandas转为arrays
+x_train=titanic_train_data_x.values
+y_train=titanic_train_data_y.values
+x_test=titanic_test_data_x.values
+
+#对每一个基学习器进行构建和预测
+rf_oof_train,rf_oof_test=get_out_fold(rf,x_train,y_train,x_test)
+ada_oof_train,ada_oof_test=get_out_fold(ada,x_train,y_train,x_test)
+et_oof_train,et_oof_test=get_out_fold(et,x_train,y_train,x_test)
+gb_oof_train,gb_oof_test=get_out_fold(gb,x_train,y_train,x_test)
+dt_oof_train,dt_oof_test=get_out_fold(dt,x_train,y_train,x_test)
+knn_oof_train,knn_oof_test=get_out_fold(knn,x_train,y_train,x_test)
+svm_oof_train,svm_oof_test=get_out_fold(svm,x_train,y_train,x_test)
+print('training is complete')
+
+x_train=np.concatenate((rf_oof_train,ada_oof_train,et_oof_train,gb_oof_train,
+                        dt_oof_train,knn_oof_train,svm_oof_train),axis=1)
+x_test=np.concatenate((rf_oof_test,ada_oof_test,et_oof_test,gb_oof_test,dt_oof_test,
+                        knn_oof_test,svm_oof_test),axis=1)
+
+#第二层学习，利用XGBoost,使用第一层预测的结果作为特征对最终的结果进行预测
+from xgboost import XGBClassifier
+
+gbm=XGBClassifier(n_estimators=2000,max_depth=4,min_child_weight=2,gamma=0.9,subsample=0.8,colsample_bytree=0.8,
+                  object='binary:logistic',nthread=-1,scale_pos_weigth=1).fit(x_train,y_train)
+predictions=gbm.predict(x_test)
+
+stackingsubmission=pd.DataFrame({'PassengersId':PassengerId,'Survived':predictions})
+stackingsubmission.to_csv('stacksubmission.csv',index=False,sep=',')
+
+
+
+
